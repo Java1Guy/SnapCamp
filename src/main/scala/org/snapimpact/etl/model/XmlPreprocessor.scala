e9d44d8898bbd55902bd49f1d2979d8bc8c9eccf
Mark Chance <mark.chance@gmail.com>
2010-05-15 19:28:47 -0600

    preprocessor using tagsoup

--- src/main/scala/org/snapimpact/etl/model/XmlPreprocessor.scala
+++ src/main/scala/org/snapimpact/etl/model/XmlPreprocessor.scala
@@ -1,4 +1,4 @@
-package org.snapimpact.etl.model
+package org.snapimpact.etl
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,78 +8,55 @@ package org.snapimpact.etl.model
  * from http://www.hars.de/2009/01/html-as-xml-in-scala.html
  */
 
-import _root_.org.xml.sax.{XMLReader,InputSource}
-import _root_.scala.xml.{Node,TopScope}
-
-import _root_.scala.xml.parsing.FactoryAdapter
-import _root_.scala.xml.factory.NodeFactory
-import _root_.scala.xml.{Elem,MetaData,NamespaceBinding,
-			 Node,Text,TopScope}
-
-trait NonBindingFactoryAdapter extends FactoryAdapter
-                               with NodeFactory[Elem] {
-
-  def nodeContainsText(localName: String) = true
-
-  // methods for NodeFactory[Elem]
-  /** constructs an instance of scala.xml.Elem */
-  protected def create(pre: String, label: String,
-                       attrs: MetaData, scpe: NamespaceBinding,
-		       children: Seq[Node]): Elem =
-    Elem( pre, label, attrs, scpe, children:_* )
-
-
-  // -- methods for FactoryAdapter
-  def createNode(pre: String, label: String,
-                 attrs: MetaData, scpe: NamespaceBinding,
-                 children: List[Node] ): Elem =
-    Elem( pre, label, attrs, scpe, children:_* )
-
-  def createText(text: String) = Text(text)
-
-  def createProcInstr(target: String, data: String) =
-    makeProcInstr(target, data)
-}
-
-trait SAXFactoryAdapter extends NonBindingFactoryAdapter {
-
-  /** The method [getReader] has to implemented by
-      concrete subclasses */
-  def getReader() : XMLReader;
-
-  override def loadXML(source : InputSource) : Node = {
-    val reader = getReader()
-    reader.setContentHandler(this)
-    scopeStack.push(TopScope)
-    reader.parse(source)
-    scopeStack.pop
-    return rootElem
-  }
-}
-
-trait HTMLFactoryAdapter extends FactoryAdapter {
-
-  val emptyElements = Set("area", "base", "br", "col", "hr", "img",
-                          "input", "link", "meta", "param")
-
-  def nodeContainsText(localName: String) =
-    !(emptyElements contains localName)
-}
-
+import model.dto.VolunteerOpportunity
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+import org.ccil.cowan.tagsoup.{XMLWriter, HTMLSchema, Parser}
+import org.xml.sax.{InputSource, ContentHandler, XMLReader}
 
-class TagSoupFactoryAdapter extends SAXFactoryAdapter
-                            with HTMLFactoryAdapter {
-  private val parserFactory = new SAXFactoryImpl
-  parserFactory.setNamespaceAware(true)
-
-  def getReader() = parserFactory.newSAXParser().getXMLReader()
-}
+import _root_.java.io.ByteArrayOutputStream
+import _root_.java.io.ByteArrayInputStream
+import _root_.java.io.OutputStreamWriter
+import xml.XML
+import net.liftweb.common.{Empty, Full}
 
 object XmlPreprocessor {
-  def preProcess(url:String) {
-    val theDoc = new TagSoupFactoryAdapter load url
-    println("Result of '"+url+"' = "+theDoc.toString)
-    theDoc
+  def preProcess(url:String) = {
+//    val theDoc = new TagSoupFactoryAdapter load url
+//    println("Result of '"+url+"' = "+theDoc.toString)
+//    theDoc
   }
+  def preProcessString(str:String) = {
+    val r: XMLReader = new Parser
+    val theSchema = new HTMLSchema
+    r.setProperty(Parser.schemaProperty, theSchema);
+    val os = new ByteArrayOutputStream(str.length)
+    val w = new OutputStreamWriter(os)
+    val x = new XMLWriter(w)
+
+    x.setPrefix(theSchema.getURI(), "")
+    r.setContentHandler(x);
+    val s = new InputSource
+    
+    s.setByteStream(new ByteArrayInputStream(str.getBytes))
+    r.parse(s);
+//    println("Result of '"+str+"' = "+os.toString)
+    val theXML = XML.loadString(os.toString)
+  }
+
+  def postProcess(vo:Option[String]) =
+  // replace <br ...> with line feed
+  // remove </br ...>
+  // replace <p> with line feed
+  // remove </p ...>
+  // remove <...>
+    if (!vo.isEmpty) {
+        Option(vo.get.replaceAll("<br[^>]*>", "\n")
+          .replaceAll("</br[^>]*>", "")
+          .replaceAll("<p[^>]*>", "\n")
+          .replaceAll("</p[^>]*>", "")
+          .replaceAll("<[^>]*>", ""))
+    } else {
+      vo
+    }
+
 }
